c2ebbbd8be07bb87b7877caaf393641ac2d7f8b0
Daniel Egorov <daniel.s.egorov@proton.me>
2022-12-15 22:05:02 -0800

    flutter format and remove unnecessary imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,3 @@
-import 'dart:ui';
-
-import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
