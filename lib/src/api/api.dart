3760586256c0bc0ae8bbdcfe45f48c82707c66c4
Daniel-Egorov <daniel.s.egorov@proton.me>
2023-01-23 11:35:23 -0800

    upsert user to users table upon login

--- lib/src/api/api.dart
+++ lib/src/api/api.dart
@@ -1,3 +1,4 @@
+import 'package:sudokgo/src/hive/hive_wrapper.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SudokGoApi {
@@ -19,5 +20,21 @@ class SudokGoApi {
 
   static Future<void> logout() async {
     await supabase.auth.signOut();
+  }
+
+  /// Create the user row in the users table
+  /// 
+  /// perform an upsert on the users table to either create or update the current
+  /// users row in the table which will ensure they have a row in the table and
+  /// that the `display_name` property in the table is in sync with the display
+  /// name stored in the [Hive]
+  static Future<void> upsertUserRow() async {
+    await supabase.from('users')
+      .upsert({
+        'created_at': supabase.auth.currentUser?.createdAt,
+        'id': supabase.auth.currentUser?.id,
+        'email': supabase.auth.currentUser?.email,
+        'display_name': HiveWrapper.getDisplayName(),
+      });
   }
 }
